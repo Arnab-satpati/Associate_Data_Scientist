cd8e4f6fdb72bdf5d5eaccd1578ee435f0f06b57
Arnab Satpati <arnab.satpati004@gmail.com>
2026-01-10 23:34:39 +0530

    done with intermediate python

---
+++ Intermediate_Python/chapter5.py
@@ -0,0 +1,77 @@
+'''
+Docstring for Intermediate_Python.chapter5
+
+This script simulates multiple random walks and visualizes them using matplotlib.
+
+# numpy and matplotlib imported, seed set.
+import numpy as np
+import matplotlib.pyplot as plt
+# initialize and populate all_walks
+all_walks = []
+for i in range(5) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
+
+
+
+# numpy and matplotlib imported, seed set
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(123)
+# clear the plot so it doesn't get cluttered if you run this many times
+plt.clf()
+
+# Simulate random walk 20 times
+all_walks = []
+for i in range(20) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.005 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
+
+'''
